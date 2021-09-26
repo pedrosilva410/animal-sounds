28b53e92e63310a5d3957f506efc21f323829df1
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2021-09-26 11:33:03 +0200

    Barebones

---
+++ src/pages/About/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+
+const About = () => {
+  return (
+    <main>
+      <h1>About</h1>
+      <p className="about__p">
+       Here you'll be able to listen to diverse animals.
+       This website exists because I was bored and wanted to code something.
+       I hope you find it interesting and have fun!
+      </p>
+    </main>
+  );
+};
+
+export default About;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Animal Sounds</h1>
+      <p className="home__p">
+        A website where you can listen to diverse animals!
+      </p>
+    </main>
+  );
+};
+
+export default Home;
